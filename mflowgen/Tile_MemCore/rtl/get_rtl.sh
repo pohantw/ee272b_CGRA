3309ad62cdd7a1bb201aa284f8da7debe4c15558
Po-Han Chen <pohan@caddyxtra.stanford.edu>
2021-07-02 22:47:42 -0700

    update RTL generation node

---
+++ mflowgen/Tile_MemCore/rtl/get_rtl.sh
@@ -0,0 +1,8 @@
+cat \
+  ../../../../caravel_user_project/caravel/verilog/rtl/defines.v      \
+  ../../../../caravel_user_project/verilog/rtl/user_project_wrapper.v \
+  ../../../../caravel_user_project/verilog/rtl/user_proj_cgra.v       \
+  ../../../../caravel_user_project/verilog/rtl/ckmux.v                \
+  ../../../../caravel_user_project/verilog/rtl/wishbone_ctl.v         \
+  ../../../../caravel_user_project/verilog/rtl/garnet_3t_4x8_nopipe.v \
+  > top.sv
